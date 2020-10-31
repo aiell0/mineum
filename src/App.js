3bd906ce85511bb726dd9460cabb22782fa59ad7
aiell0 <lawrence.aiello@laiello.com>
2020-10-31 09:17:24 -0400

    Add simple logout button; still not working fully

--- src/App.js
+++ src/App.js
@@ -1,57 +1,39 @@
-import React, { useState } from "react";
+/* eslint-disable max-len */
+import {React, useState} from 'react';
 import './App.css';
-import GoogleLogin from 'react-google-login';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import {GoogleLogin, GoogleLogout} from 'react-google-login';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 
 const Dashboard = () => {
-  // const authInstance = window.gapi.auth2.getAuthInstance()
-  // const user = authInstance.currentUser.get()
-  // const profile = user.getBasicProfile()
-  // const email = profile.getEmail()
-  // const imageUrl = profile.getImageUrl()
-
   return (
     <>
-      {/* <nav>
-        <div>BookFace</div>
-        <img className="push" src={imageUrl} />
-        <Dropdown>
-          <Dropdown.Toggle>
-            {email}
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item onClick={authInstance.signOut}>Sign out</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </nav> */}
       <div className="container">
-        <p>Look at this bookface</p>
-        <img src="https://christianlauersen.files.wordpress.com/2015/11/img_0423.png" />
+        <GoogleLogout
+          clientId="313696716932-2i8kgp5113dhrptkqbhhjjli379bhhe5.apps.googleusercontent.com"
+          buttonText="Logout"
+          onLogoutSuccess={() => {}}
+        />,
       </div>
     </>
-  )
-}
+  );
+};
 
+/**
+ * Main React App.
+ * @return {React}
+ */
 function App() {
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [url, setUrl] = useState("");
-  const [userSignedIn, setUserSignedIn] = useState(false);
-
-  const responseGoogle = response => {
-    setName(response.profileObj.name);
-    setEmail(response.profileObj.email);
-    setUrl(response.profileObj.imageUrl);
-    setUserSignedIn(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const responseGoogle = (response) => {
+    setIsLoggedIn(true);
     console.log(response);
-  }
+  };
 
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          {userSignedIn ? <Redirect to="/dashboard" /> :
+          {isLoggedIn ? <Redirect to="/dashboard" /> :
             <div className="App">
               <h1>Mineum</h1>
               <GoogleLogin
@@ -65,10 +47,10 @@ function App() {
           }
         </Route>
         <Route path="/dashboard">
-          <Dashboard />
+          <Dashboard setIsLoggedIn />
         </Route>
       </Switch>
-    </BrowserRouter>
+    </BrowserRouter >
   );
 }
 
