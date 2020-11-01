550078760630ea6e9f8990eda09e7be4d429818c
aiell0 <laiello@2ndwatch.com>
2020-11-01 12:39:33 -0500

    Added logout button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 /* eslint-disable max-len */
+/* eslint-disable indent */
+/* eslint-disable no-tabs */
+/* eslint-disable react/prop-types */
 import {React, useState} from 'react';
 import './App.css';
 import {GoogleLogin, GoogleLogout} from 'react-google-login';
-import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
+import {Switch, Route, Redirect, useHistory} from 'react-router-dom';
+require('dotenv').config();
 
-const Dashboard = () => {
-  return (
-    <>
-      <div className="container">
-        <GoogleLogout
-          clientId="313696716932-2i8kgp5113dhrptkqbhhjjli379bhhe5.apps.googleusercontent.com"
-          buttonText="Logout"
-          onLogoutSuccess={() => {}}
-        />,
+/**
+ * User Dashboard Page.
+ * @param {function} onLogout - callback to logout function in App.
+ * @return {React} Google Logout Button.
+ */
+function Dashboard({onLogout}) {
+	return (
+		<>
+			<div className="container">
+				<GoogleLogout
+					clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+					buttonText="Logout"
+					onLogoutSuccess={onLogout}
+				/>,
       </div>
-    </>
-  );
+		</>
+	);
 };
 
 /**
@@ -23,35 +32,43 @@ const Dashboard = () => {
  * @return {React}
  */
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const responseGoogle = (response) => {
-    setIsLoggedIn(true);
-    console.log(response);
-  };
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const history = useHistory();
+	const handleLogin = (response) => {
+		setIsLoggedIn(true);
+		console.log('Logged in to Google.');
+		console.log(response);
+	};
+	const handleLogout = () => {
+		setIsLoggedIn(false);
+		console.log('Logged out of Google.');
+		history.push('/');
+	};
+	const handleGoogleFail = () => {
+		console.log('ya dun fucked up');
+	};
 
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          {isLoggedIn ? <Redirect to="/dashboard" /> :
-            <div className="App">
-              <h1>Mineum</h1>
-              <GoogleLogin
-                clientId="313696716932-2i8kgp5113dhrptkqbhhjjli379bhhe5.apps.googleusercontent.com"
-                buttonText="Login"
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-                cookiePolicy={'single_host_origin'}
-              />,
+	return (
+		<Switch>
+			<Route exact path="/">
+				{isLoggedIn ? <Redirect to="/dashboard" /> :
+					<div className="App">
+						<h1>Mineum</h1>
+						<GoogleLogin
+							clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+							buttonText="Login"
+							onSuccess={handleLogin}
+							onFailure={handleGoogleFail}
+							cookiePolicy={'single_host_origin'}
+						/>,
             </div>
-          }
-        </Route>
-        <Route path="/dashboard">
-          <Dashboard setIsLoggedIn />
-        </Route>
-      </Switch>
-    </BrowserRouter >
-  );
+				}
+			</Route>
+			<Route path="/dashboard">
+				<Dashboard onLogout={handleLogout} />
+			</Route>
+		</Switch>
+	);
 }
 
 export default App;
