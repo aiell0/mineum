3a6096fe74065ee07227c7c09b7eb6ac288a3e1c
aiell0 <lawrence.aiello@laiello.com>
2020-11-10 21:36:09 -0500

    Added footer

--- src/App.js
+++ src/App.js
@@ -263,6 +263,7 @@ function App() {
             <Rewards />
             <SocialMedia />
             <Settings />
+            <Footer />
           </div>
         }
       </Route >
@@ -970,6 +971,31 @@ function Settings() {
         </div>
       </div>
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <Navbar type="dark" theme="primary" expand={'lg'} className="bg-dark">
+        <Container>
+          <img src={navLogo} width="auto" height="75" style={{opacity: 0.5}} className="mr-2" alt="Mineum virtual mobile mining" />
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Miner <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://discord.gg/yQMmmFw" target="_blank" rel="noreferrer">Support</a>
+              </li>
+            </ul>
+          </div>
+        </Container>
+      </Navbar>
+    </footer>
   );
 }
 
