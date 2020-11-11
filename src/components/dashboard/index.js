1c8ee084bab050b5692566241962eb73198eddd6
aiell0 <lawrence.aiello@laiello.com>
2020-11-11 10:05:20 -0500

    implement stop/start mining buttons

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -95,10 +95,33 @@ export default function Dashboard({onLogout, firstName, lastName, googleId}) {
   const baseRewardRate = .0000001;
   const [reward, setReward] = useState(0);
   const [rewardsThisEpoch, setRewardsThisEpoch] = useState(0);
+  const [timer, setTimer] = useState(0);
+  const [formattedTime, setFormattedTime] = useState('00:00:00');
+  const [isMining, setIsMining] = useState(false);
 
   const handleOnIdle = (event) => {
+    console.log('user has gone idle');
+    setIsMining(false);
     updateRewards(googleId, reward);
     onLogout();
+  };
+
+  const handleStart = () => {
+    console.log('start mining');
+    setIsMining(true);
+  };
+
+  const handleStop = () => {
+    console.log('stop mining');
+    setIsMining(false);
+  };
+
+  const formatTime = () => {
+    const getSeconds = `0${(timer % 60)}`.slice(-2);
+    const minutes = `${Math.floor(timer / 60)}`;
+    const getMinutes = `0${minutes % 60}`.slice(-2);
+    const getHours = `0${Math.floor(timer / 3600)}`.slice(-2);
+    return `${getHours}:${getMinutes}:${getSeconds}`;
   };
 
   const {getElapsedTime} = useIdleTimer({
@@ -124,11 +147,15 @@ export default function Dashboard({onLogout, firstName, lastName, googleId}) {
     });
 
     const interval = setInterval(() => {
+      if (isMining) {
+        setTimer(timer + 1);
+      }
       setReward(baseRewardRate * getElapsedTime());
+      setFormattedTime(formatTime());
     }, 1000);
     // cleanup
     return () => clearInterval(interval);
-  }, [firstName, lastName, googleId]);
+  }, [firstName, lastName, googleId, timer, isMining]);
 
   return (
     <>
@@ -142,18 +169,29 @@ export default function Dashboard({onLogout, firstName, lastName, googleId}) {
                 <CardBody>
                   <img src={gnomeIdleGif} style={{opacity: 0.7, float: 'right'}} width="auto" height="75" className="mr-2" alt="Mineum virtual mobile mining" />
                   <CardText>
-                    <i className="fas fa-clock"></i>Mined time: <b>00:00:00</b> <br /> <font size="2" style={{color: '#DDDDDD'}}>Counts up your minded time</font>
-                    <Progress bar value='75' style={{width: '75%'}}></Progress>
+                    <i className="fas fa-clock"></i>Mined time: {formattedTime} <br />
+                    <Progress bar value='0' style={{width: '75%'}}></Progress>
                   </CardText>
                   <CardText>
-                    <i className="fas fa-trophy"></i>Your current rank: <b>#24</b> <br /> <font size="2" style={{color: '#DDDDDD'}}><b>#1</b> user343, <b>#2</b> @user345, <b>#3</b> user123</font>
+                    <i className="fas fa-trophy"></i>Your current rank: <b>#24</b> <br />
                   </CardText>
                   <CardText>
                     <i className="fas fa-users"></i>Active users: <b>219</b>
                   </CardText>
-                  <ButtonGroup>
-                    <Button outline theme="success">START MINING</Button>
-                    <Button outline theme="danger">STOP MINING</Button>
+                  <ButtonGroup> {
+                    isMining ? (
+                      <>
+                        <Button outline theme='dark' disabled={true}>START MINING</Button>
+                        <Button outline theme='danger' onClick={handleStop}>STOP MINING</Button>
+                      </>
+                    ) :
+                      (
+                        <>
+                          <Button outline theme='success' onClick={handleStart}>START MINING</Button>
+                          <Button outline theme='dark' disabled={true}>STOP MINING</Button>
+                        </>
+                      )
+                  }
                   </ButtonGroup>
                 </CardBody>
               </Card>
